515cc326c33d16d7310ca5cc9355f15d36d0e1ba
Shradha <shradha.sgl@gmail.com>
2020-01-23 20:34:57 +0530

    Halo

---
+++ ains.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h> 
+#include <stdio.h> 
+
+struct timespec start, end;
+
+void fillData(int a[], int len) 
+{ 
+    srand(time(0));
+    for (int i = 0 ; i < len ; i++) 
+    	a[i] = rand()%100;
+    
+    return; 
+} 
+
+void SingleFunction_MergeSort(int arr[], int n) 
+{ 
+	int curr_size, left_start, right_end, i , j, k, mid, n1, n2;
+
+	for (curr_size = 1; curr_size <= n-1; curr_size = curr_size * 2)  
+	{ 
+		for (left_start = 0; left_start < n-1; left_start += curr_size * 2) 
+		{ 
+			mid = ((left_start + curr_size - 1) < ( n-1)) ? (left_start + curr_size - 1) : (n-1);
+			right_end = ((left_start + (curr_size * 2) - 1) < (n-1)) ? (left_start + (curr_size * 2) - 1) : (n-1);
+			
+			i, j, k; 
+			n1 = mid - left_start + 1; 
+			n2 = right_end - mid; 
+
+			int *L = malloc(n1 * sizeof(int));
+			int *R = malloc(n2 * sizeof(int));
+
+			for (i = 0; i < n1; i++) 
+				L[i] = arr[left_start + i]; 
+			for (j = 0; j < n2; j++) 
+				R[j] = arr[mid + 1+ j]; 
+
+			i = 0; 
+			j = 0;  
+			k = left_start; 
+			
+			while (i < n1 && j < n2) 
+			{ 
+				if (L[i] <= R[j]) 
+				{ 
+					arr[k] = L[i]; 
+					i++; 
+				} 
+				else
+				{ 
+					arr[k] = R[j]; 
+					j++; 
+				} 
+				k++; 
+			} 
+
+			while (i < n1) 
+			{ 
+				arr[k] = L[i]; 
+				i++; 
+				k++; 
+			} 
+
+			while (j < n2) 
+			{ 
+				arr[k] = R[j]; 
+				j++; 
+				k++; 
+			} 
+		} 
+	} 
+} 
+
+int main() 
+{
+	int max = 1000000, tests = 1, flag = 1;
+	double Avg_SingleFunc = 0;
+	
+	int *a = malloc(max * sizeof(int));
+
+	for(int i = 0 ; i < tests ; i++)
+	{	
+
+		fillData(a, max);
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		SingleFunction_MergeSort(a, max);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		
+		double tdiff = (end.tv_sec - start.tv_sec) + 1e-9*(end.tv_nsec - start.tv_nsec);
+		
+		for(int x=0 ; x<max-1 ; x++)
+			if(a[x] > a[x+1])
+				flag = 0;
+		
+		Avg_SingleFunc = tdiff;
+	}
+	
+	if(flag == 1)
+		printf("\nAverage: function inling, time %f\n", Avg_SingleFunc);
+	else
+		printf("Sort Failed\n");
+
+	return 0;
+} 
--- pls.c
+++ pls.c
@@ -22,45 +22,39 @@ int* merge_sort(int *arr, int n)
         for (left_start=0; left_start < n-1; left_start += (curr_size << 1)) 
         {
             int x = left_start + curr_size - 1, y = left_start + (curr_size << 1) - 1, z = n -1;
+            
             int mid = (x < z)? x: z;
             int right_end = (y < z)? y: z;
+            
             int i, j, k; 
             int m = mid, l = left_start, r = right_end;
+            
             int n1 = m - l + 1; 
             int n2 =  r - m; 
             int L[n1], R[n2]; 
+            
             for (i = 0; i < n1; i++) 
                 L[i] = arr[l + i]; 
 
             for (j = 0; j < n2; j++) 
                 R[j] = arr[m + 1+ j]; 
         
-            /* Merge the temp arrays back into arr[l..r]*/
             i = 0; 
             j = 0; 
             k = l; 
-             while (i < n1 && j < n2) 
+            
+            while (i < n1 && j < n2) 
             { 
                 int cmp = (L[i] <= R[j]);
                 int min = R[j] ^ ((R[j] ^ L[i]) & (-cmp));
                 i += cmp, j += !cmp;
             } 
         
-            /* Copy the remaining elements of L[], if there are any */
             while (i < n1) 
-            { 
                 arr[k++] = L[i++]; 
-                // i++; 
-                // k++; 
-            } 
         
-            /* Copy the remaining elements of R[], if there are any */
             while (j < n2) 
-            { 
                 arr[k++] = R[j++]; 
-                // j++; 
-                // k++; 
-            } 
 
         } 
     }  
