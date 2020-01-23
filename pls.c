aa0327422220535c4f3fe9a94993c9ca6283ec5b
Shradha <shradha.sgl@gmail.com>
2020-01-23 19:08:21 +0530

    Branching removed

--- pls.c
+++ pls.c
@@ -14,57 +14,6 @@ void fillData(int *a, int len)
     return; 
 }   
 
-void merge(int arr[], int l, int m, int r) 
-{ 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
-  
-    /* create temp arrays */
-    int L[n1], R[n2]; 
-  
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
-  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; 
-    j = 0; 
-    k = l; 
-    while (i < n1 && j < n2) 
-    { 
-        if (L[i] <= R[j]) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of L[], if there are any */
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of R[], if there are any */
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
-} 
-
 int* merge_sort(int *arr, int n)
 {
     int curr_size, left_start;
@@ -72,10 +21,47 @@ int* merge_sort(int *arr, int n)
     { 
         for (left_start=0; left_start<n-1; left_start += 2*curr_size) 
         {
-            // int x = left_start + curr_size - 1, y = 
-            int mid = (left_start + curr_size - 1 < n-1)?left_start + curr_size - 1: n-1;
-            int right_end = (left_start + 2*curr_size - 1 < n-1)?left_start + 2*curr_size - 1: n-1;
-            merge(arr, left_start, mid, right_end); 
+            int x = left_start + curr_size - 1, y = left_start + 2*curr_size - 1, z = n -1;
+            int mid = (x < z)? x: z;
+            int right_end = (y < z)? y: z;
+            int i, j, k; 
+            int m = mid, l = left_start, r = right_end;
+            int n1 = m - l + 1; 
+            int n2 =  r - m; 
+            int L[n1], R[n2]; 
+            for (i = 0; i < n1; i++) 
+                L[i] = arr[l + i]; 
+
+            for (j = 0; j < n2; j++) 
+                R[j] = arr[m + 1+ j]; 
+        
+            /* Merge the temp arrays back into arr[l..r]*/
+            i = 0; 
+            j = 0; 
+            k = l; 
+             while (i < n1 && j < n2) 
+            { 
+                int cmp = (L[i] <= R[j]);
+                int min = R[j] ^ ((R[j] ^ L[i]) & (-cmp));
+                i += cmp, j += !cmp;
+            } 
+        
+            /* Copy the remaining elements of L[], if there are any */
+            while (i < n1) 
+            { 
+                arr[k++] = L[i++]; 
+                // i++; 
+                // k++; 
+            } 
+        
+            /* Copy the remaining elements of R[], if there are any */
+            while (j < n2) 
+            { 
+                arr[k++] = R[j++]; 
+                // j++; 
+                // k++; 
+            } 
+
         } 
     }  
 	
